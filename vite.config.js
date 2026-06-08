d19261edf5cd76ed7136987cf1e107df274cdc35
Chester <iphix09@gmail.com>
2026-06-08 16:06:15 +0300

    files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-export default {
-  server: {
-    proxy: {
-      "/api/football": {
-        target: "https://api.football-data.org/v4",
-        rewrite: (path) => path.replace(/^\/api\/football/, ""),
-        changeOrigin: true,
-        headers: { "X-Auth-Token": "0d73bad5c3114f288747caef2d389819" },
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api/football": {
+          target: "https://api.football-data.org/v4",
+          rewrite: (path) => path.replace(/^\/api\/football/, ""),
+          changeOrigin: true,
+          headers: { "X-Auth-Token": env.API_FOOTBALL_KEY },
+        },
       },
     },
-  },
-};
+  };
+});
